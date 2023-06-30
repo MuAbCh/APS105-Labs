2e79c69913988d6bf5ee0b48f59e8d67691b279a
MuAbCh <118654828+MuAbCh@users.noreply.github.com>
2023-06-30 11:52:42 -0600

    Update lab5part2.c

--- lab5part2.c
+++ lab5part2.c
@@ -1,5 +1,31 @@
 //Getting the Statistics of the Game
 
+#include <stdio.h>
+
+void appendStatistics(int userChoice[], const int ChoicesNum, int histogram[]);
+int frequentBox(int histogram[], const int BoxesNum);
+
+int main(void) {
+  const int BoxesNum = 20;
+  const int ChoicesNum = 5;
+  int userOne[ChoicesNum], userOneHisto[BoxesNum];
+  for (int i = 0; i < BoxesNum; i++) {
+    userOneHisto[i] = 0;
+  }
+  int numOfTimes = 0;
+  while (numOfTimes <= 5) {
+    printf("Enter choices: ");
+    for (int i = 0; i < 5; i++) {
+      scanf("%d", &userOne[i]);
+    }
+    appendStatistics(userOne, ChoicesNum, userOneHisto);
+    numOfTimes++;
+  }
+  printf("The smallest and most frequently used box is: %d\n",
+         frequentBox(userOneHisto, BoxesNum));
+  return 0;
+}
+
 void appendStatistics(int userChoice[], const int ChoicesNum, int histogram[]) {
   // Fill in the histogram with the number of times each index was picked
   for (int x = 0; x < ChoicesNum; x++) {
@@ -10,6 +36,11 @@ void appendStatistics(int userChoice[], const int ChoicesNum, int histogram[]) {
       }
     }
   }
+  // Print the histogram
+  for (int counter = 0; counter < 20; counter++) {
+    printf("%d: %d, ", counter, histogram[counter]);
+  }
+  printf("\n");
 }
 
 int frequentBox(int histogram[], const int BoxesNum) {
@@ -28,3 +59,4 @@ int frequentBox(int histogram[], const int BoxesNum) {
   }
   return GP;
 }
+
