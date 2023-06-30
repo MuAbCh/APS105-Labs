3b8572a7201c599a8cc2aaa27c0e5fbbbf8f2f45
MuAbCh <118654828+MuAbCh@users.noreply.github.com>
2023-06-30 11:22:50 -0600

    Create lab1part1.c

---
+++ lab1part1.c
@@ -0,0 +1,7 @@
+#include <stdio.h> //need to get scanf and printf functions
+
+int main(void) {
+    //now we start our printing
+    printf("C uses escape sequences for a variety of purposes.\nSome common ones are:\n     to print \", use \\\"\n     to print \\, use \\\\\n     to jump to a new line, use \\n\n");
+    return 0; //to confirm porgram works
+}
