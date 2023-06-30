3da274f110829d6248f93586fab5373dfa9a56b0
MuAbCh <118654828+MuAbCh@users.noreply.github.com>
2023-06-30 11:23:34 -0600

    Create lab1part2.c

---
+++ lab1part2.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void) {
+
+    double inputCR = 0, inputAmount = 0, outputAmount = 0;
+
+    printf("Enter the conversion rate: ");
+    scanf("%lf", &inputCR);
+    printf("Enter the amount to be converted (in foreign currency): ");
+    scanf("%lf", &inputAmount);
+
+    outputAmount = inputCR * inputAmount;
+    printf("The amount in Canadian Dollars is: %.2lf\n", outputAmount);
+
+}
